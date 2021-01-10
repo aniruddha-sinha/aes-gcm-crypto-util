d132926647b4b1a23d7c7c44c31e1405b100ba59
aniruddha-sinha <Aniruddha.Sinha@bitwiseglobal.com>
2021-01-10 21:03:03 +0530

    Added 16 bit IV

--- src/main/java/com/crypto/aes/password/core/AESConstants.java
+++ src/main/java/com/crypto/aes/password/core/AESConstants.java
@@ -10,7 +10,7 @@ public class AESConstants {
 	public static final String ENCRYPT_ALGO = "AES/GCM/NoPadding";
 
 	public static final int TAG_LENGTH_BIT = 128; // must be one of {128, 120, 112, 104, 96}
-	public static final int IV_LENGTH_BYTE = 12;
+	public static final int IV_LENGTH_BYTE = 16;
 	public static final int SALT_LENGTH_BYTE = 16;
 	public static final Charset UTF_8 = StandardCharsets.UTF_8;
 	public static final String SECRET_KEY_TRANSFORMATION_ALGORITHM = "PBKDF2WithHmacSHA256";
